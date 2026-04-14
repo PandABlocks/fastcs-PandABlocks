1413dd5b9f8bc0f66c2e4636117a5c782180fc06
Hiran Wijesinghe <hiran.wijesinghe@diamond.ac.uk>
2026-04-14 11:45:48 +0000

    fix container build CI

--- src/fastcs_pandablocks/__main__.py
+++ src/fastcs_pandablocks/__main__.py
@@ -6,5 +6,10 @@ from fastcs_pandablocks.panda.panda_controller import PandaController
 
 from ._version import __version__
 
-if __name__ == "__main__":
+
+def main() -> None:
     launch(PandaController, version=__version__)
+
+
+if __name__ == "__main__":
+    main()

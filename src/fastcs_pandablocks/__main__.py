3f83b2641158d40a4e8e7f14ff30c07e7baa248c
Tom Kane <thomas.kane@diamond.ac.uk>
2026-08-17 16:14:44 +0000

    update to use fastcs 0.14 api

--- src/fastcs_pandablocks/__main__.py
+++ src/fastcs_pandablocks/__main__.py
@@ -1,15 +1,8 @@
 """Interface for ``python -m fastcs_pandablocks``."""
 
-from fastcs.launch import launch
+from fastcs import launch
 
+from fastcs_pandablocks import __version__
 from fastcs_pandablocks.panda.panda_controller import PandaController
 
-from ._version import __version__
-
-
-def main() -> None:
-    launch(PandaController, version=__version__)
-
-
-if __name__ == "__main__":
-    main()
+launch(controller_classes=PandaController, version=__version__)
